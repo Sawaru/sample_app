95d8636d56badab5681f0db0f80280ff7e9e6791
Sawaru <44Alucard44@mail.ru>
2015-08-20 16:08:22 +1000

    Add precompiled assets for Heroku

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module SampleApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
     config.secret_key_base = YAML.load(File.open("#{Rails.root}/config/secrets.yml"))[Rails.env]['secret_key_base']
   end
 end
